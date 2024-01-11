7ed239b54541e5473ab5558045928897505ddbc6
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-01-11 21:53:09 +0200

    task 2 without throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const ST_KEY = "videoplayer-current-time"
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+// get current time
+const timeApdater = (data) => {
+    localStorage.setItem(ST_KEY, data.seconds);
+}
+
+player.on('timeupdate', timeApdater);
+
+// set current time
+const savedTime = localStorage.getItem(ST_KEY) || 0;
+player.setCurrentTime(savedTime);
