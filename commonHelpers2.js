38b015b52f9b19f1397852cd129051dbf6a3a255
MaximNoritsyn <34163031+MaximNoritsyn@users.noreply.github.com>
2024-01-11 19:53:29 +0000

    Deploying to gh-pages from @ MaximNoritsyn/goit-js-hw-08@7ed239b54541e5473ab5558045928897505ddbc6 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r}from"./assets/vendor-443c30ec.js";const e="videoplayer-current-time",m=document.querySelector("iframe"),t=new r(m),a=o=>{localStorage.setItem(e,o.seconds)};t.on("timeupdate",a);const c=localStorage.getItem(e)||0;t.setCurrentTime(c);
+//# sourceMappingURL=commonHelpers2.js.map
