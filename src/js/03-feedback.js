bbb4a342ba649be99b1a71046bea2eca7f48ad33
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-01-13 12:54:59 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const onFormInput = event => {
+    const target = event.target;
+    formData[target.name] = target.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+};
+
+const onFormSubmit = event => {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+    event.currentTarget.reset();
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+const populateForm = () => {
+    const savedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (savedData) {
+        Object.keys(savedData).forEach(name => {
+            formData[name] = savedData[name];
+            form.elements[name].value = savedData[name];
+        });
+    }
+}
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+form.addEventListener('submit', onFormSubmit);
+
+populateForm();
